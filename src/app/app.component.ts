653abaa5837f64a983f43f9dffb63ef845cb2989
odevjava <kevinandersonpaulino@gmail.com>
2024-10-30 13:17:57 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  NgbAccordionBody,
+  NgbAccordionButton, NgbAccordionCollapse,
+  NgbAccordionDirective,
+  NgbAccordionHeader,
+  NgbAccordionItem
+} from "@ng-bootstrap/ng-bootstrap";
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+
+@Component({
+  selector: 'wac-root',
+  standalone: true,
+  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionButton, NgbAccordionBody, NgbAccordionCollapse, FaIconComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  title = 'whatsapp-clone-front';
+
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
+}
